c590c7ae35c474728818990d8852aa55d3fc2d19
FranciscoRafael <francisco.carneiro14@uol.com.br>
2017-04-27 11:41:03 -0300

    bertocco é chato pra caralho

--- projeto1/client_tcp.c
+++ projeto1/client_tcp.c
@@ -1,37 +1,53 @@
-************************** 
-******** Esqueleto 2 ***** 
-************************** 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <netdb.h>
 
-#define LISTEN_PORT 12345
+#define LISTEN_PORT 5400
 #define MAX_PENDING 5
 #define MAX_LINE 256
 
 int main() {
-        struct sockaddr_in socket_address;
-        char buf[MAX_LINE];
-        unsigned int len;
-        int s, new_s;
+	
+	struct hostent *host_address;
+  	struct sockaddr_in socket_address;
+   	char buf[MAX_LINE];
+   	unsigned int len;
+   	int socket_fd, new_s;
 
+	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	if (socket_fd == -1) { 
+		printf("Erro ao abrir socket\n");
+		exit(1);
+	}
+	else if(socket_fd > 0) { 
+		printf("Correto\n"); 
+	}
+
+		
+	host_address = gethostbyname(argv[1]); 
+	
+
+	while(1) { 
         /* criaÃ§Ã£o da estrutura de dados de endereÃ§o */
-        bzero((char *)&socket_address, sizeof(socket_address));
-	(...)
+  	bzero((char *)&socket_address, sizeof(socket_address));
+
 
         /* criaÃ§Ã£o de socket passivo */
-	(...)
+
 
 	/* Associar socket ao descritor */
-	(...)
+
 
 	/* Criar escuta do socket para aceitar conexÃµes */
-	(...)
+
 
         /* aguardar/aceita conexÃ£o, receber e imprimir texto na tela, enviar eco */
-	(...)
+
+	}
+
 }
 
 
--- projeto1/server_tcp.c
+++ projeto1/server_tcp.c
@@ -1,42 +1,104 @@
-************************** 
-******** Esqueleto 1 *****
-************************** 
+/*************************** /
+/********* Esqueleto 1 *****/
+/***************************/
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <netdb.h>
 
-#define SERVER_PORT 12345
+#define SERVER_PORT 5400
 #define MAX_LINE 256
 
 int main(int argc, char * argv[]) {
-        struct hostent *host_address;
-        struct sockaddr_in socket_address;
-        char *host;
-        char buf[MAX_LINE];
-        int s;
-        int len;
+      
+	struct hostent *host_address;
+ 	struct sockaddr_in socket_address, client_address; 
+   	char *host;
+   	char buffer[MAX_LINE];
+   	int len_client, client_acc; 
+	int socket_fd, bind_r, rc, w; 
 
 
-	/* verificaÃ§Ã£o de argumentos */
-	(...)
+	socket_fd= socket(AF_INET, SOCK_STREAM, 0);
+	
+	if (socket_fd == -1) { 
+		printf("Erro ao abrir socket\n");
+		exit(1);
+	}
+	else if(socket_fd > 0) { 
+		printf("Correto\n"); 
+	}
 
-	/* traduÃ§Ã£o de nome para endereÃ§o IP */
-	(...)
+	bzero((char *)&socket_address, sizeof(socket_address));
 
-        /* criaÃ§Ã£o da estrutura de dados de endereÃ§o */
-        bzero((char *)&socket_address, sizeof(socket_address));
-	(...)
+	socket_address.sin_family = AF_INET;
+	socket_address.sin_port = htons(SERVER_PORT); 
+	socket_address.sin_addr.s_addr = INADDR_ANY; 
 
-        /* criaÃ§Ã£o de socket ativo*/
-	(...)
+	 
+	bind_r = bind(socket_fd, (struct sockaddr*)&socket_address, sizeof(socket_address));
 
-	/* estabelecimento da conexÃ£o */
-	(...)
+	if(bind_r == -1) { 
+		printf("Erro bind\n"); 
+		exit(1);
+	}
+	else { 
+		printf("Bind correto\n"); 
+	}
+	if(listen(socket_fd, 3)  == -1) { 
+		printf("Erro listen\n"); 
+		exit(1); 
+	}
+	else { 
+		printf("Correto listen\n"); 
+	}
 
-        /* ler e enviar linhas de texto, receber eco */
-	(...)
+	len_client = sizeof(struct sockaddr_in); 
+	
+
+
+	while (1) { 
+		client_acc = accept(socket_fd, (struct sockaddr*)&client_address, (socklen_t *)&client_acc); 
+
+ 		if(client_acc == -1) {
+			printf("Cliente não aceito\n"); 
+			exit(1); 
+		}
+		else { 
+			printf("Cliente aceito\n"); 
+		}
+		
+		bzero(buffer, MAX_LINE);
+		
+		rc = read(client_acc, buffer, MAX_LINE); 
+		
+		if(rc == 0) {
+			printf("Conexão Terminada\n"); 
+			close(client_acc);
+		}
+
+		if(rc == -1) { 
+			printf("Erro ao receber a mensagem do cliente\n"); 
+			exit(1); 
+		}
+		else { 
+			printf("Mensagem: %s\n", buffer);
+		
+		}
+		
+		w = write(client_acc, buffer, MAX_LINE); 	
+		if(w == -1) {
+			printf("Erro de escrita no socket\n"); 
+			exit(1); 
+		}
+			
+	}
+
+	close(socket_fd); 
+	
+	return 0;
+
 
 }
 
